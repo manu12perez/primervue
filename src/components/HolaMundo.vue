5b7f70a45001ab563a47416b4f8c49e706bb8f7a
manu12perez <manu12perez@gmail.com>
2024-10-24 14:17:32 +0200

    HolaMundo

--- src/components/HolaMundo.vue
+++ src/components/HolaMundo.vue
@@ -1,17 +1,19 @@
 <template>
-    <div>
-        <h1>Hola Mundo</h1>
-    </div>
+  <div>
+    <h1>Hola Mundo</h1>
+  </div>
 </template>
 
 <script>
-    //ESTO ES CODIGO JS (ESPECIAL)
-    //CADA COMPONENT QUE CREEMOS SIEMPRE DEBEMOS EXPORTARLO CON UN name
-    export default {
-        name: "HolaMundo",
-    }
+//ESTO ES CODIGO JS (ESPECIAL)
+//CADA COMPONENT QUE CREEMOS SIEMPRE DEBEMOS EXPORTARLO CON UN name
+export default {
+  name: "HolaMundo",
+};
 </script>
 
 <style>
-    h1 {color: blue}
+h1 {
+  color: blue;
+}
 </style>
